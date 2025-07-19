c1c38f2f9ad05f838a032cd632fbfbfd6906114d
hyeoz <jenny970203@gmail.com>
2025-07-19 22:07:53 +0900

    fix: lint 에러 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  settings: {
+    'import/resolver': {
+      'babel-module': {
+        alias: {
+          '@': './src'
+        }
+      }
+    }
+  },
+  // 다음 규칙은 babel-plugin-module-resolver를 사용하는 경우 발생하는 ESLint 경고를 무시합니다
+  rules: {
+    'import/no-unresolved': 'off'
+  }
 };
